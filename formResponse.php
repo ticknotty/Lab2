14e0603df9bc6711260a9ab805ba9b415f8997a9
ticknotty <nicktotty8@gmail.com>
2015-10-26 16:30:46 +0000

    Added radio button for selecting gender

--- formResponse.php
+++ formResponse.php
@@ -11,7 +11,12 @@ $surname = $_POST["surname"];
 $day = $_POST["day"];
 $month = $_POST["month"];
 $year = $_POST["year"];
+$male = $_POST["male"];
+$female = $_POST["female"];
 echo "Name: " . $forename . " " . $surname . "<br>";
 
 echo "DOB: " . $day . "/" . $month . "/" . $year , "<br>";
+
+echo $male;
+echo $female;
 ?>
