44a6895e063bbe20781617fbc56386dda4174e7d
ticknotty <nicktotty8@gmail.com>
2015-10-26 16:35:52 +0000

    Outputting the radio button result

--- formResponse.php
+++ formResponse.php
@@ -1,4 +1,3 @@
-<div class='sex_div'>
 <?php
 /**
  * Created by PhpStorm.
@@ -12,20 +11,12 @@ $surname = $_POST["surname"];
 $day = $_POST["day"];
 $month = $_POST["month"];
 $year = $_POST["year"];
-
+$male = $_POST["male"];
+$female = $_POST["female"];
 echo "Name: " . $forename . " " . $surname . "<br>";
 
 echo "DOB: " . $day . "/" . $month . "/" . $year , "<br>";
 
-if($sex=='male') {?>
-    <span class='sex_style'>
-            <?php echo $row['sex']; ?>
-        </span>
-    <?php if($job_type=='female') {?>
-        <span class='sex_style2'>
-            <?php echo $row['sex']; ?>
-        </span>
-    <?php } ?>
-<?php } ?>
-</div>
+echo $male;
+echo $female;
 ?>
